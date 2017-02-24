0d2a073776d62ceb23e1b17e47c699ac494a37ca
maccyber <jonas.enge@gmail.com>
2017-02-24 12:22:37 +0100

    Adds post-data

--- config/index.js
+++ config/index.js
@@ -5,5 +5,12 @@ module.exports = {
   ORIGIN_URL: process.env.ORIGIN_URL || 'https://forms.cs.next.t-fk.win',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.micro.t-fk.no'
+  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.micro.t-fk.no',
+  SERVICES: [
+    {
+      name: 'CS',
+      url: process.env.CS_URL || 'https://cs.services.t-fk.no',
+      path: process.env.CS_PATH || '/createTicket'
+    }
+  ]
 }
