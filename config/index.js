511e8d198f4ef758dcf2e59b4cb271f547347aee
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-27 12:16:51 +0100

    Changes SESSION_STORAGE_URL

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   ORIGIN_URL: process.env.ORIGIN_URL || 'https://forms.cs.next.t-fk.win',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
-  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.micro.t-fk.no',
+  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.service.t-fk.no',
   SERVICES: [
     {
       name: 'CS',
