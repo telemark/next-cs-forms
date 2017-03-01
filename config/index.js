6cce5a6f36d71369e857dc852a11b3397cd3ab5d
maccyber <jonas.enge@gmail.com>
2017-03-01 14:32:01 +0100

    Changed orgin url

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   SSO_URL: process.env.SSO_URL || 'https://sso.router.t-fk.win',
-  ORIGIN_URL: process.env.ORIGIN_URL || 'https://forms.cs.next.t-fk.win',
+  ORIGIN_URL: process.env.ORIGIN_URL || 'https://cs.next.t-fk.no',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.service.t-fk.no',
