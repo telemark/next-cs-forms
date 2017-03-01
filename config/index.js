8d9da742851e68838dfe0d15a5fbfdeabe56d03a
maccyber <jonas.enge@gmail.com>
2017-03-01 13:00:40 +0100

    Rewrite cs-config

--- config/index.js
+++ config/index.js
@@ -6,11 +6,6 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   ENCRYPTOR_SECRET: process.env.ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.service.t-fk.no',
-  SERVICES: [
-    {
-      name: 'CS',
-      url: process.env.CS_URL || 'https://cs-service.t-fk.no',
-      path: process.env.CS_PATH || '/createTicket'
-    }
-  ]
+  url: process.env.CS_URL || 'https://cs-service.t-fk.no',
+  path: process.env.CS_PATH || '/createTicket'
 }
