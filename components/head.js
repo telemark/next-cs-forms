fe5276c2141973f0290ebe5d8a5a249b55384300
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-01 14:15:33 +0100

    polyfill.io

--- components/head.js
+++ components/head.js
@@ -6,13 +6,14 @@ import Head from 'next/head'
 export default () => (
   <div>
     <Head>
-      <title>Forms cs</title>
+      <title>Forms CS</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link href='//cdn.muicss.com/mui-0.9.9-rc2/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
       <link href='static/css/app.css' rel='stylesheet' type='text/css' media='screen' />
       <link rel='icon' sizes='192x192' href='static/images/icons/chrome-touch-icon-192x192.png' />
       <link rel='apple-touch-icon' href='static/images/icons/apple-touch-icon-precomposed.png' />
       <link rel='shortcut icon' href='static/images/icons/favicon.ico' />
+      <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
     </Head>
   </div>
 )
